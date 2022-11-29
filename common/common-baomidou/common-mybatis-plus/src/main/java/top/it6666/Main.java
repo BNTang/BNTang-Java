a67780a9a8a02fb2269182edbf3b1de018d9710d
BNTang <303158131@qq.com>
2022-11-30 01:03:00 +0800

    add mybatis plus add hutool 然后做了分模块

---
+++ common/common-baomidou/common-mybatis-plus/src/main/java/top/it6666/Main.java
@@ -0,0 +1,14 @@
+package top.it6666;
+
+/**
+ * @author BNTang
+ * @version V1.0
+ * @project ${PROJECT_NAME}
+ * @date Created in ${DATE} ${TIME}
+ * @description ${description}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

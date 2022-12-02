91c1497c4af170af68de972e4b78dfa2c6aa8458
BNTang <303158131@qq.com>
2022-12-02 20:00:15 +0800

    add file

---
+++ service/service-baomidou/service-mybatis-plus/src/test/java/top/it6666/generator/GeneratorTest.java
@@ -0,0 +1,81 @@
+package top.it6666.generator;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.fill.Column;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootTest
+class GeneratorTest {
+    @Test
+    void testFastGenerator() {
+        FastAutoGenerator.create("url", "username", "password")
+                .globalConfig(builder -> {
+                    // 设置作者
+                    builder.author("BNTang")
+                            // 开启 swagger 模式
+                            .enableSwagger()
+                            // 覆盖已生成文件
+                            .fileOverride()
+                            // 指定输出目录
+                            .outputDir("D://");
+                })
+                .packageConfig(builder -> {
+                    // 设置父包名
+                    builder.parent("top.it6666.pojo.generator")
+                            // 设置父包模块名
+                            .moduleName("system")
+                            // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D://"));
+                })
+                .strategyConfig(builder -> {
+                    // 设置需要生成的表名
+                    builder.addInclude("t_simple")
+                            // 设置过滤表前缀
+                            .addTablePrefix("t_", "c_");
+                })
+                // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                //.templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+
+    @Test
+    void testInteractiveGenerator() {
+        FastAutoGenerator.create("url", "http://", "http://")
+                // 全局配置
+                .globalConfig((scanner, builder) ->
+                        builder.author(scanner.apply("请输入作者名称？"))
+                                .fileOverride())
+                // 包配置
+                .packageConfig((scanner, builder) -> builder.parent(scanner.apply("请输入包名？")))
+                // 策略配置
+                .strategyConfig((scanner, builder) ->
+                        builder.addInclude(getTables(scanner.apply("请输入表名，多个英文逗号分隔？所有输入 all")))
+                                .controllerBuilder()
+                                .enableRestStyle()
+                                .enableHyphenStyle()
+                                .entityBuilder()
+                                .enableLombok()
+                                .addTableFills(
+                                        new Column("create_time", FieldFill.INSERT))
+                                .build()
+                )
+                //模板引擎配置，默认 Velocity 可选模板引擎 Beetl 或 Freemarker
+                //.templateEngine(new BeetlTemplateEngine())
+                //.templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+
+    // 处理 all 情况
+    protected static List<String> getTables(String tables) {
+        return "all".equals(tables) ? Collections.emptyList() : Arrays.asList(tables.split(","));
+    }
+}

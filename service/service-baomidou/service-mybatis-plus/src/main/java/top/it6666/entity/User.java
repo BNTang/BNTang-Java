d84a7cae8a0b4b7031f935698cc377c9984b74d3
BNTang <303158131@qq.com>
2022-12-05 10:15:35 +0800

    add file

--- service/service-baomidou/service-mybatis-plus/src/main/java/top/it6666/entity/User.java
+++ service/service-baomidou/service-mybatis-plus/src/main/java/top/it6666/entity/User.java
@@ -1,21 +1,24 @@
 package top.it6666.entity;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.io.Serializable;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author BNTang
  * @since 2022-12-03
  */
+@Data
 @TableName("user")
-@ApiModel(value = "User对象", description = "")
+@ApiModel(value = "User对象", description = "用户实体类")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -32,42 +35,24 @@ public class User implements Serializable {
     @ApiModelProperty("邮箱")
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-            "id=" + id +
-            ", name=" + name +
-            ", age=" + age +
-            ", email=" + email +
-        "}";
-    }
+    /**
+     * mybatisplus 的逻辑删除
+     * 说明:
+     * 只对自动注入的 sql 起效:
+     * 插入: 不作限制
+     * 查找: 追加 where 条件过滤掉已删除数据,如果使用 wrapper.entity 生成的 where 条件也会自动追加该字段
+     * 更新: 追加 where 条件防止更新到已删除数据,如果使用 wrapper.entity 生成的 where 条件也会自动追加该字段
+     * 删除: 转变为 更新
+     * 例如:
+     * 删除: update user set deleted=1 where id = 1 and deleted=0
+     * 查找: select id,name,deleted from user where deleted=0
+     * 字段类型支持说明:
+     * 支持所有数据类型(推荐使用 Integer,Boolean,LocalDateTime)
+     * 如果数据库字段使用datetime,逻辑未删除值和已删除值支持配置为字符串null,另一个值支持配置为函数来获取值如now()
+     * 附录:
+     * 逻辑删除是为了方便数据恢复和保护数据本身价值等等的一种方案，但实际就是删除。
+     * 如果你需要频繁查出来看就不应使用逻辑删除，而是以一个状态去表示。
+     */
+    //@TableLogic
+    //private Integer deleted;
 }

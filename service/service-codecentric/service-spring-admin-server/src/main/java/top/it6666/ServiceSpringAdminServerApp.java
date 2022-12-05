3a1487d80e1f68b0df9938e40f219df29a73fae7
BNTang <303158131@qq.com>
2022-12-05 11:23:36 +0800

    add file

--- service/service-codecentric/service-spring-admin-server/src/main/java/top/it6666/ServiceSpringAdminServerApp.java
+++ service/service-codecentric/service-spring-admin-server/src/main/java/top/it6666/ServiceSpringAdminServerApp.java
@@ -2,12 +2,10 @@ package top.it6666;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableAdminServer
+@SpringBootApplication
 public class ServiceSpringAdminServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ServiceSpringAdminServerApp.class, args);

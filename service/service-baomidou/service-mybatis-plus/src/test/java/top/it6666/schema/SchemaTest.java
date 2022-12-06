4ff11b86d7ce60dfc247d83f1240fe180d9707df
BNTang <303158131@qq.com>
2022-12-06 17:48:29 +0800

    :bento: 添加或更新资源

---
+++ service/service-baomidou/service-mybatis-plus/src/test/java/top/it6666/schema/SchemaTest.java
@@ -0,0 +1,45 @@
+//package top.it6666.schema;
+//
+//import cn.hutool.core.lang.Assert;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import top.it6666.ServiceMyBatisPlusApp;
+//import top.it6666.mapper.CustomerMapper;
+//import top.it6666.mapper.SchemaUserMapper;
+//import top.it6666.schema.Customer;
+//import top.it6666.schema.User;
+//
+//import javax.annotation.Resource;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.List;
+//
+///**
+// * @author BNTang
+// * @version V1.0
+// * @project BNTang-Java
+// * @date Created in 2022/12/1 23:41
+// * @description
+// **/
+//@SpringBootTest(classes = ServiceMyBatisPlusApp.class)
+//class SchemaTest {
+//    @Resource
+//    private SchemaUserMapper schemaUserMapper;
+//    @Resource
+//    private CustomerMapper customerMapper;
+//
+//    @Test
+//    void testCustomerSelect() {
+//        List<Customer> customerList = this.customerMapper.selectList(null);
+//        Assert.equals(7, customerList.size());
+//        customerList.forEach(System.out::println);
+//    }
+//
+//    @Test
+//    void testUserSelect() {
+//        List<User> userList = this.schemaUserMapper.selectList(null);
+//        Assert.equals(5, userList.size());
+//        userList.forEach(System.out::println);
+//    }
+//}

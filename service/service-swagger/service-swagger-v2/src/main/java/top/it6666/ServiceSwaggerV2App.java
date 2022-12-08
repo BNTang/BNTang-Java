9166ee16f4f37c0d59517f8ee7b399728a00e43a
BNTang <303158131@qq.com>
2022-12-08 11:33:11 +0800

    add new file

--- service/service-swagger/service-swagger-v2/src/main/java/top/it6666/ServiceSwaggerV2App.java
+++ service/service-swagger/service-swagger-v2/src/main/java/top/it6666/ServiceSwaggerV2App.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableSwagger2Doc
 @EnableKnife4j
-public class ServiceSwaggerApp {
+public class ServiceSwaggerV2App {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceSwaggerApp.class, args);
+        SpringApplication.run(ServiceSwaggerV2App.class, args);
     }
 }
---
+++ service/service-swagger/service-swagger-v3/src/main/java/top/it6666/ServiceSwaggerV3App.java
@@ -0,0 +1,16 @@
+package top.it6666;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author BNTang
+ * @version 1.0
+ * @since ${YEAR}-${HOUR}-${DAY}
+ **/
+@SpringBootApplication
+public class ServiceSwaggerV3App {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSwaggerV3App.class, args);
+    }
+}

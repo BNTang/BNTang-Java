1f6ae655ffd4c3557020b416800a3128bb6a94af
BNTang <303158131@qq.com>
2023-01-08 22:26:41 +0800

    add code

---
+++ service/service-dromara/service-hutool/src/main/java/top/it6666/core/clone/Student.java
@@ -0,0 +1,15 @@
+package top.it6666.core.clone;
+
+import lombok.Data;
+
+/**
+ * @author BNTang
+ * @version 1.0
+ * @description
+ * @since 2023-15-08
+ **/
+@Data
+public class Student {
+    private String name;
+    private Integer age;
+}
---
+++ service/service-dromara/service-hutool/src/test/java/top/it6666/core/clone/TObjectUtil.java
@@ -0,0 +1,27 @@
+package top.it6666.core.clone;
+
+import cn.hutool.core.util.ObjectUtil;
+
+/**
+ * @author BNTang
+ * @version 1.0
+ * @description
+ * @since 2023-15-08
+ **/
+public class TObjectUtil {
+    public static void main(String[] args) {
+        testClone();
+    }
+
+    public static void testClone() {
+        DogByDeepClone dogByDeepClone = new DogByDeepClone();
+        System.out.println("dogByDeepClone.hashCode() = " + dogByDeepClone.hashCode());
+
+        DogByDeepClone clone = ObjectUtil.clone(dogByDeepClone);
+        System.out.println("clone.hashCode() = " + clone.hashCode());
+
+        Student student = new Student();
+        Student studentClone = ObjectUtil.clone(student);
+        System.out.println(studentClone);
+    }
+}
